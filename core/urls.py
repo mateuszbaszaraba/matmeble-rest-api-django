c8808b4afae0c156d461e9635f808ec95940d63f
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-03-01 16:24:14 +0100

    Included auth urls in core

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/', include('authentication.urls', namespace='users')),
 ]
 
